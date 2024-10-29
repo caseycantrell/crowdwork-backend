b2ba1aa0c04c554621a39ed0241aaa48663360b3
Casey Cantrell <cantrellcasey@gmail.com>
2024-10-29 11:01:12 -0500

    db config

--- src/server.ts
+++ src/server.ts
@@ -3,43 +3,43 @@ import dotenv from 'dotenv';
 import { initializeSocket } from './socket';
 import app from './app';
 
-// load env vars
+// Load environment variables
 dotenv.config();
 
 console.log('Backend URL:', process.env.BACKEND_URL);
 
-// validate required env vars
-if (!process.env.DB_USER || !process.env.DB_HOST || !process.env.DB_NAME || !process.env.DB_PASSWORD || !process.env.DB_PORT) {
-  throw new Error('DB env variables are not set correctly');
+// Validate that DATABASE_URL is set
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not set correctly');
 }
 
 const PORT = process.env.PORT || 3002;
 
-// create HTTP server
+// Create HTTP server
 const server = http.createServer(app);
 
-// initialize socket.io
+// Initialize socket.io
 initializeSocket(server);
 
-// start the server
+// Start the server
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  }).on('error', (err: Error) => {
-    console.error('Failed to start server:', err);
-  }).on('close', () => {
-    console.log('Server connection closed.');
-  });
-  
-  // handle unhandled rejections and uncaught exceptions
-  process.on('unhandledRejection', (reason: any, promise: Promise<any>) => {
-    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
-  });
-  
-  process.on('uncaughtException', (error: Error) => {
-    console.error('Uncaught Exception:', error.message);
-    console.error('Error Stack:', error.stack);
-  });
-  
-  process.on('exit', (code) => {
-    console.log(`Process exited with code: ${code}`);
-  });
+  console.log(`Server running on port ${PORT}`);
+}).on('error', (err: Error) => {
+  console.error('Failed to start server:', err);
+}).on('close', () => {
+  console.log('Server connection closed.');
+});
+
+// Handle unhandled rejections and uncaught exceptions
+process.on('unhandledRejection', (reason: any, promise: Promise<any>) => {
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+});
+
+process.on('uncaughtException', (error: Error) => {
+  console.error('Uncaught Exception:', error.message);
+  console.error('Error Stack:', error.stack);
+});
+
+process.on('exit', (code) => {
+  console.log(`Process exited with code: ${code}`);
+});
