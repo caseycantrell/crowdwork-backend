f26636bdc491a2a7c4cdbc040f5d7ba53e765a27
Casey Cantrell <cantrellcasey@gmail.com>
2024-10-29 10:55:10 -0500

    db config

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,13 +4,23 @@ import { Pool } from 'pg';
 // load environment variables from .env
 dotenv.config();
 
-export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT as string, 10),
-});
+// determine the connection string based on the environment
+const connectionString = process.env.DATABASE_URL || '';
+
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString,
+        ssl: { rejectUnauthorized: false }, // required for Heroku Postgres
+      }
+    : {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: parseInt(process.env.DB_PORT as string, 10),
+      }
+);
 
 pool.on('connect', () => {
   console.log('New jabroni connected to Postgres');
@@ -29,3 +39,5 @@ export const connectDB = async () => {
     process.exit(1); 
   }
 };
+
+export { pool };
